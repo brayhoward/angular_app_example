983c7c793722469709c79f60ad836aa6018e2f84
brayhoward <brandonrhoward@gmail.com>
2017-12-19 12:16:54 -0600

    Finish accordion cmpt.

--- web-angular/src/app/cmpts/accordion-with-toggle/accordion-with-toggle.component.ts
+++ web-angular/src/app/cmpts/accordion-with-toggle/accordion-with-toggle.component.ts
@@ -6,22 +6,30 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./accordion-with-toggle.component.scss']
 })
 export class AccordionWithToggleComponent implements OnInit {
-  add: String = "px-utl:add";
-  remove: String = "px-utl:remove";
-  icon: String = this.add;
-  expanded: boolean = false;
+  private add: String = "px-utl:add";
+  private remove: String = "px-utl:remove";
+  private icon: String = this.add;
+  private hasToggleSwitch: boolean = false;
 
   @Input() headerValue: String
   @Input() toggleSwitchCallback: (event: MouseEvent) => void
+  @Input() expanded: boolean = false;
+  @Input() checked: boolean;
 
 
   ngOnInit() {
+    const checked = this.checked;
+    this.hasToggleSwitch = !!this.toggleSwitchCallback;
+
+    // Defaulted checked to equal true.
+    this.checked = (checked === undefined) ? true : checked
   }
 
   toggleExpanded() {
     this.expanded = !this.expanded;
     const { add, remove, expanded } = this;
 
+    // Toggle the open/close icon
     this.icon = expanded ? remove : add;
 
     return expanded
--- web-angular/src/app/cmpts/report-detail/report-detail.component.ts
+++ web-angular/src/app/cmpts/report-detail/report-detail.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./report-detail.component.scss']
 })
 export class ReportDetailComponent implements OnInit {
-
-  constructor() { }
+  private showLubrication: boolean = true;
+  private showFuel: boolean = true;
+  private showLabor: boolean = true;
 
   ngOnInit() {
   }
 
+  private toggleLubrication = (event) => {
+    // this.showLubrication = event.detail.value;
+    this.toggleProperty('showLubrication')(event)
+  }
+
+  private toggleFuel = (event) => {
+    this.toggleProperty('showFuel')(event)
+  }
+
+  private toggleLabor = (event) => {
+    this.toggleProperty('showLabor')(event)
+  }
+
+  private toggleProperty = (prop) => (event) => {
+    this[prop] = event.detail.value;
+    //TODO: remove console.log
+    console.log(prop + ':', this[prop])
+  }
 }
