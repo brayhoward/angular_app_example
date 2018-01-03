205c0ec41d5cab7f5361f8bd4aef3320448622c5
brayhoward <brandonrhoward@gmail.com>
2018-01-03 12:13:18 -0600

    Add px-simple-area-chart.

--- web/src/app/cmpts/engine-lifecycle/engine-lifecycle.component.ts
+++ web/src/app/cmpts/engine-lifecycle/engine-lifecycle.component.ts
@@ -6,8 +6,103 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./engine-lifecycle.component.scss']
 })
 export class EngineLifecycleComponent implements OnInit {
-  constructor() { }
+  chartData: any;
+  chartConfig = {
+    "fistSeries": {
+      "name": "Parts",
+      "x": "x",
+      "y": "y",
+      "yAxisUnit": "$"
+    },
+    "secondSeries": {
+      "name": "Labor",
+      "x": "x",
+      "y": "y1",
+      "yAxisUnit": "$"
+    },
+    "thirdSeries": {
+      "name": "Fuel",
+      "x": "x",
+      "y": "y2",
+      "yAxisUnit": "$"
+    },
+    "fourthSeries": {
+      "name": "Lube",
+      "x": "x",
+      "y": "y3",
+      "yAxisUnit": "$"
+    }
+  }
 
   ngOnInit() {
+    this.chartData = [
+      {
+        "x": 0,
+        "y3": 0,
+        "y2": 0,
+        "y1": 0,
+        "y": 0
+      }, {
+        "x": 4,
+        "y3": 50,
+        "y2": 45,
+        "y1": 55,
+        "y": 60
+      }, {
+        "x": 8,
+        "y3": 50,
+        "y2": 45,
+        "y1": 55,
+        "y": 60
+      }, {
+        "x": 12,
+        "y3": 50,
+        "y2": 45,
+        "y1": 55,
+        "y": 60
+      }, {
+        "x": 16,
+        "y3": 50,
+        "y2": 45,
+        "y1": 55,
+        "y": 60
+      }, {
+        "x": 20,
+        "y2": 45,
+        "y1": 75,
+        "y": 70
+      }, {
+        "x": 24,
+        "y2": 45,
+        "y1": 55,
+        "y": 62
+      }, {
+        "x": 28,
+        "y3": 50,
+        "y2": 45,
+        "y1": 55,
+        "y": 62
+      }, {
+        "x": 32,
+        "y2": 45,
+        "y1": 55,
+        "y": 62
+      }, {
+        "x": 36,
+        "y2": 45,
+        "y1": 55,
+        "y": 62
+      }, {
+        "x": 40,
+        "y2": 45,
+        "y1": 55,
+        "y": 62
+      }, {
+        "x": 44,
+        "y2": 45,
+        "y1": 55,
+        "y": 62
+      }
+    ]
   }
 }
