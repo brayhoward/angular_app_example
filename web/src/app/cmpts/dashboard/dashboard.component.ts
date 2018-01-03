77305c6c025a496aba1d5e201635dda61051fc04
brayhoward <brandonrhoward@gmail.com>
2018-01-02 20:42:05 -0600

    Wire up dashboard buttons and use call-to-action cmpts in place of buttons.

--- web/src/app/cmpts/dashboard/dashboard.component.ts
+++ web/src/app/cmpts/dashboard/dashboard.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { ReportFormComponent } from '../report-form/report-form.component';
 
 @Component({
   selector: 'dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  constructor(
+    private dialog: MatDialog,
+    private router: Router
+  ) { }
+
+  onNewReport() {
+    this.onViewReports();
+
+    this.dialog.open(ReportFormComponent);
+  }
+
+  onViewReports() {
+    this.router.navigateByUrl('/reports');
+  }
+}
