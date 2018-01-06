246d7368d4a5c79b5d8547c7a3aae956ec1f8775
brayhoward <brandonrhoward@gmail.com>
2018-01-06 12:21:04 -0600

    Fix service intervals table toggle logic.

--- web/src/app/cmpts/service-intervals/service-intervals.component.ts
+++ web/src/app/cmpts/service-intervals/service-intervals.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewChecked } from '@angular/core';
+import { Component, AfterViewChecked. OnInit } from '@angular/core';
 import { CurrencyPipe } from '@angular/common';
 import { MatTableDataSource } from '@angular/material';
 import { SelectionModel } from '@angular/cdk/collections';
@@ -16,15 +16,22 @@ declare var Morris: any;
   templateUrl: './service-intervals.component.html',
   providers: [ CurrencyPipe ]
 })
-export class ServiceIntervalsComponent implements AfterViewChecked {
+export class ServiceIntervalsComponent implements AfterViewChecked, OnInit {
   displayedColumns = ['select', 'service', 'interval', 'parts', 'labor', 'price'];
-  dataSource = new MatTableDataSource<Element>(ELEMENT_DATA);
-  selection = new SelectionModel<Element>(true, []);
+  dataSource: any;
   chartNotInitialized = true;
+  selectedRows: number[]
 
   constructor(
     private currency: CurrencyPipe
   ) { }
+
+  ngOnInit() {
+    const services = ELEMENT_DATA;
+
+    this.selectedRows = services.map( service => service.id )
+    this.dataSource = new MatTableDataSource(services);
+  }
 
   ngAfterViewChecked() {
     isElementRendered('intervals-chart') && this.chartNotInitialized && this.initChart();
@@ -79,25 +86,25 @@ export class ServiceIntervalsComponent implements AfterViewChecked {
   }
 
   cashOut = (num) => this.currency.transform(num);
+
+  toggleRow({ detail: { value }}, row) {
+    const { isSelected, selectedRows, } = this;
+
+    this.selectedRows = value ? [...this.selectedRows, row.id] : selectedRows.filter(id => id !== row.id)
+  }
+
+  isSelected = ({ id }) => this.selectedRows.includes(id);
 }
 
-export interface Element {
-  service: string;
-  interval: number;
-  parts: number;
-  labor: number;
-  price:number;
-}
-
-const ELEMENT_DATA: Array<Element> = [
-  {service: 'Commissioning Parts', interval: 0, parts: 1521, labor: 0, price: 1521},
-  {service: 'Emergency Spare Parts', interval: 0, parts: 21758, labor: 0, price: 21758},
-  {service: 'Lube Oil Change', interval: 6000, parts: 581, labor: 200, price: 1031},
-  {service: 'Planned Service A', interval: 4000, parts: 635, labor: 600, price: 1235},
-  {service: 'Planned Service B', interval: 8000, parts: 1756, labor: 1155, price: 2911},
-  {service: 'Planned Service C', interval: 16000, parts: 1521, labor: 750, price: 4577},
-  {service: 'Top End Overhaul', interval: 24000, parts: 20468, labor: 16260, price: 36728},
-  {service: 'Bottom End Overhaul', interval: 48000, parts: 56708, labor: 26835, price: 83543}
+const ELEMENT_DATA = [
+  {service: 'Commissioning Parts', interval: 0, parts: 1521, labor: 0, price: 1521, id: 1},
+  {service: 'Emergency Spare Parts', interval: 0, parts: 21758, labor: 0, price: 21758, id: 2},
+  {service: 'Lube Oil Change', interval: 6000, parts: 581, labor: 200, price: 1031, id: 3},
+  {service: 'Planned Service A', interval: 4000, parts: 635, labor: 600, price: 1235, id: 4},
+  {service: 'Planned Service B', interval: 8000, parts: 1756, labor: 1155, price: 2911, id: 5},
+  {service: 'Planned Service C', interval: 16000, parts: 1521, labor: 750, price: 4577, id: 6},
+  {service: 'Top End Overhaul', interval: 24000, parts: 20468, labor: 16260, price: 36728, id: 7},
+  {service: 'Bottom End Overhaul', interval: 48000, parts: 56708, labor: 26835, price: 8354, id: 4}
 ];
 
 // Hack because angular lifecycle methods are seriously lacking
